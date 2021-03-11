4720994e29949137cdac3407b2ce27ac35827ac2
Natalia <naguado@gmail.com>
2021-03-12 00:31:12 +0100

    Utilizo Map para generar las tablas

--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
-function createTable(num) {
-  let tabla = document.createElement('table');
-  let cabecera = document.createElement('thead');
-  let cabecerFila = document.createElement('tr');
+const numerosTabla = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  let cabeceraColumna = document.createElement('th');
-  cabeceraColumna.colSpan = '2';
-  cabeceraColumna.innerHTML = `Tabla del ${num}`;
-
-  cabecerFila.appendChild(cabeceraColumna);
-  cabecera.appendChild(cabecerFila);
-  tabla.appendChild(cabecera);
-
-  let cuerpo = document.createElement('tbody');
-  for (let cont = 0; cont <= 10; cont++) {
+let generarabla = function (num, cuerpo) {
+  numerosTabla.map((num2) => {
     let fila = document.createElement('tr');
 
     let columnaOperandos = document.createElement('td');
-    columnaOperandos.innerHTML = `${num} &times; ${cont} `;
+    columnaOperandos.innerHTML = `${num} &times; ${num2} `;
     fila.appendChild(columnaOperandos);
 
     let columnaResultado = document.createElement('td');
-    let resultado = document.createTextNode(`${num * cont}`);
+    let resultado = document.createTextNode(`${num * num2}`);
     columnaResultado.appendChild(resultado);
     fila.appendChild(columnaResultado);
 
     cuerpo.appendChild(fila);
-  }
-  tabla.appendChild(cuerpo);
-  return tabla;
-}
+  });
+};
 
 function createTables() {
-  for (let num = 1; num < 10; num++) {
-    let tabla = createTable(num);
+
+  numerosTabla.map((num) => {
+    let tabla = document.createElement('table');
+    let cabecera = document.createElement('thead');
+    let cabecerFila = document.createElement('tr');
+
+    let cabeceraColumna = document.createElement('th');
+    cabeceraColumna.colSpan = '2';
+    cabeceraColumna.innerHTML = `Tabla del ${num}`;
+
+    cabecerFila.appendChild(cabeceraColumna);
+    cabecera.appendChild(cabecerFila);
+    tabla.appendChild(cabecera);
+    let cuerpo = document.createElement('tbody');
+    generarabla(num, cuerpo);
+    tabla.appendChild(cuerpo);
     document.body.appendChild(tabla);
-  }
+    num++;
+  });
 }
 
 createTables();
