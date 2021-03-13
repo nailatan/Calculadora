8d38e92e80c688184155bdae881c0cc87dce9857
Natalia <naguado@gmail.com>
2021-03-13 17:41:54 +0100

    Mejorar utilizando funciones

--- src/index.js
+++ src/index.js
@@ -1,42 +1,45 @@
 const numerosTabla = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let generarabla = function (num, cuerpo) {
-  numerosTabla.map((num2) => {
-    let fila = document.createElement('tr');
+function createHeadTD(num)  {
+  let cabeceraColumna = document.createElement('th');
+  cabeceraColumna.colSpan = '2';
+  cabeceraColumna.innerHTML = `Tabla del ${num}`;
+  return cabeceraColumna;
+}
 
+let createHead = function(num) {  
+  let cabecera = document.createElement('thead');
+  let fila = document.createElement('tr');
+  fila.appendChild(createHeadTD(num));
+  cabecera.appendChild(fila);
+  return cabecera;
+}
+
+function createBody(num) {
+  let cuerpo = document.createElement('tbody');
+  
+  numerosTabla.map((item) => {
+    let fila = document.createElement('tr');
     let columnaOperandos = document.createElement('td');
-    columnaOperandos.innerHTML = `${num} &times; ${num2} `;
+    
+    columnaOperandos.innerHTML = `${num} &times; ${item} `;
     fila.appendChild(columnaOperandos);
 
     let columnaResultado = document.createElement('td');
-    let resultado = document.createTextNode(`${num * num2}`);
-    columnaResultado.appendChild(resultado);
+    columnaResultado.appendChild(document.createTextNode(`${num * item}`));    
     fila.appendChild(columnaResultado);
-
-    cuerpo.appendChild(fila);
+    cuerpo.append(fila)    
   });
+  return cuerpo;
+}
+let createTable = (num) =>{
+  let tabla = document.createElement('table');
+  tabla.append(createHead(num),createBody(num));
+   return tabla;
 };
 
 function createTables() {
-
-  numerosTabla.map((num) => {
-    let tabla = document.createElement('table');
-    let cabecera = document.createElement('thead');
-    let cabecerFila = document.createElement('tr');
-
-    let cabeceraColumna = document.createElement('th');
-    cabeceraColumna.colSpan = '2';
-    cabeceraColumna.innerHTML = `Tabla del ${num}`;
-
-    cabecerFila.appendChild(cabeceraColumna);
-    cabecera.appendChild(cabecerFila);
-    tabla.appendChild(cabecera);
-    let cuerpo = document.createElement('tbody');
-    generarabla(num, cuerpo);
-    tabla.appendChild(cuerpo);
-    document.body.appendChild(tabla);
-    num++;
-  });
+  numerosTabla.map((num) => {document.body.append(createTable(num))});
 }
 
 createTables();
