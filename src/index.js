bf82ec6db648e44bf6c78374939fbb694bfd7202
Natalia <naguado@gmail.com>
2021-03-13 17:59:40 +0100

    Unificando la creación de td

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 const numerosTabla = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function createHeadTD(num)  {
+function createElementHeadTD(titulo)  {
   let cabeceraColumna = document.createElement('th');
   cabeceraColumna.colSpan = '2';
-  cabeceraColumna.innerHTML = `Tabla del ${num}`;
+  cabeceraColumna.innerHTML = titulo;
   return cabeceraColumna;
 }
 
 let createHead = function(num) {  
   let cabecera = document.createElement('thead');
   let fila = document.createElement('tr');
-  fila.appendChild(createHeadTD(num));
+  fila.appendChild(createElementHeadTD(`Tabla del ${num}`));
   cabecera.appendChild(fila);
   return cabecera;
 }
 
+function createElementTD(valor) {
+  let columnaOperandos = document.createElement('td');
+  columnaOperandos.innerHTML = valor;
+  return columnaOperandos;
+
+}
 function createBody(num) {
   let cuerpo = document.createElement('tbody');
   
   numerosTabla.map((item) => {
     let fila = document.createElement('tr');
-    let columnaOperandos = document.createElement('td');
-    
-    columnaOperandos.innerHTML = `${num} &times; ${item} `;
-    fila.appendChild(columnaOperandos);
-
-    let columnaResultado = document.createElement('td');
-    columnaResultado.appendChild(document.createTextNode(`${num * item}`));    
-    fila.appendChild(columnaResultado);
+    fila.append(createElementTD(`${num} &times; ${item} `),createElementTD(`${num * item}`));
     cuerpo.append(fila)    
   });
   return cuerpo;
 }
+
 let createTable = (num) =>{
   let tabla = document.createElement('table');
   tabla.append(createHead(num),createBody(num));
